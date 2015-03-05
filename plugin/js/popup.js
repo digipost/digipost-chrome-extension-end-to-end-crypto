bf6bcad42ce9ae0998f5120ac52af80b4639f7f7
Jøran Vagnby Lillesand <lillesand@gmail.com>
2015-03-05 17:01:53 +0100

    Added use strict to files. Avoid using global namespace, even though not really important in the popups limited context

--- plugin/js/popup.js
+++ plugin/js/popup.js
@@ -1,7 +1,10 @@
+window.addEventListener('load', function() {
+
+"use strict";
+
 var errorField;
 var keyDataField;
 
-window.addEventListener('load', function() {
 	sendMessage('hasKey?');
 	click('setKeyButton', function() {
     sendMessage('setKey', keyDataField.value);
@@ -12,38 +15,40 @@ window.addEventListener('load', function() {
 
   keyDataField = document.getElementById('keydata');
   errorField = document.querySelector('.error');
-}, false);
 
-function click(id, callback) {
-	document.getElementById(id).addEventListener('click', callback, false);
-}
-
-function toggle(hasKey) {
-  if (hasKey) {
-    keyDataField.value = '';
-    hideError();
+  function click(id, callback) {
+    document.getElementById(id).addEventListener('click', callback, false);
   }
 
-	document.getElementById('hasKey').style.display   = hasKey ? 'block' : 'none';
-	document.getElementById('needsKey').style.display = hasKey ? 'none' : 'block';
-}
-
-function showError(message) {
-  errorField.innerText = message;
-  errorField.classList.remove('hidden')
-}
-
-function hideError() {
-  errorField.classList.add('hidden')
-}
-
-function sendMessage(message, data) {
-	chrome.runtime.sendMessage({ to: 'background', message: message, data: data }, function(response) {
-    if (response.error) {
-      toggle(false);
-      showError(response.error);
+  function toggle(hasKey) {
+    if (hasKey) {
+      keyDataField.value = '';
+      hideError();
     }
-		toggle(response.hasKey)
-	});
 
-}
+    document.getElementById('hasKey').style.display   = hasKey ? 'block' : 'none';
+    document.getElementById('needsKey').style.display = hasKey ? 'none' : 'block';
+  }
+
+  function showError(message) {
+    errorField.innerText = message;
+    errorField.classList.remove('hidden')
+  }
+
+  function hideError() {
+    errorField.classList.add('hidden')
+  }
+
+  function sendMessage(message, data) {
+    chrome.runtime.sendMessage({ to: 'background', message: message, data: data }, function(response) {
+      if (response.error) {
+        toggle(false);
+        showError(response.error);
+      }
+      toggle(response.hasKey)
+    });
+
+  }
+
+});
+
