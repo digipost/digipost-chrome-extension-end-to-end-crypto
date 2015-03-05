5f5280142198f37401099f9dde745fe9c5d7bc40
Jøran Vagnby Lillesand <lillesand@gmail.com>
2015-03-05 17:41:37 +0100

    Wrapped override.js in anonymous function to avoid polluting global namespace. Use strict

--- plugin/js/override.js
+++ plugin/js/override.js
@@ -1,50 +1,55 @@
 console.log("Overriding Digipost JavaScript functions");
 
-if (dp.url) {
-	dp.url.real_normalize = dp.url.normalize;
-	dp.url.normalize = function(url) {
-		if (url.indexOf("blob:") === 0) {
-			return url;
-		}
-		return dp.url.real_normalize(url);
-	}
-}
+(function() {
 
-if (dp.embed) {
-	dp.embed.real_html = dp.embed.html;
-	dp.embed.html = function(options) {
-		var options = options || {};
-		options.sandbox = '';
-		return dp.embed.real_html(options);
-	}
-}
+  "use strict";
 
-if (dp.views.content) {
-	dp.views.content.init_original = dp.views.content.init;
-	dp.views.content.init = function() {
-		var view = this;
-		dp.spinner.show();
-		document.addEventListener('decrypted', process);
-		document.addEventListener('decryption-failed', failed);
+  if (dp.url) {
+    dp.url.real_normalize = dp.url.normalize;
+    dp.url.normalize = function(url) {
+      if (url.indexOf("blob:") === 0) {
+        return url;
+      }
+      return dp.url.real_normalize(url);
+    }
+  }
 
-		function process(data){
-			dp.spinner.hide();
-			document.removeEventListener('decrypted', process);
-			view.doc.contentUri = function() {
-				return data.detail.url;
-			};
-			view.imageUrl = view.doc.contentUri;
-			dp.views.content.init_original.apply(view);
-			dp.sizes.update($(".doc"));
-			$(".meta h2 a").attr('href', data.detail.url);
-			$("a[download]").attr('href', data.detail.url);
-		}
-		function failed(data) {
-			console.log(data);
-			dp.spinner.hide();
-			dp.modal.show(data.detail.error);
-		}
-		document.dispatchEvent(new CustomEvent('start', { detail: this.doc.contentUri() } ));
-	};
-	console.log("Injected...");
-}
+  if (dp.embed) {
+    dp.embed.real_html = dp.embed.html;
+    dp.embed.html = function(options) {
+      var options = options || {};
+      options.sandbox = '';
+      return dp.embed.real_html(options);
+    }
+  }
+
+  if (dp.views.content) {
+    dp.views.content.init_original = dp.views.content.init;
+    dp.views.content.init = function() {
+      var view = this;
+      dp.spinner.show();
+      document.addEventListener('decrypted', show);
+      document.addEventListener('decryption-failed', failed);
+      document.dispatchEvent(new CustomEvent('start', { detail: this.doc.contentUri() } ));
+
+      function show(data){
+        dp.spinner.hide();
+        document.removeEventListener('decrypted', show);
+        view.doc.contentUri = function() {
+          return data.detail.url;
+        };
+        view.imageUrl = view.doc.contentUri;
+        dp.views.content.init_original.apply(view);
+        dp.sizes.update($(".doc"));
+        $(".meta h2 a").attr('href', data.detail.url);
+        $("a[download]").attr('href', data.detail.url);
+      }
+
+      function failed(data) {
+        dp.spinner.hide();
+        dp.modal.show(data.detail.error);
+      }
+    };
+  }
+
+})();
