f6d41b545fe65aabcd53530a2f58ffdc4a77fc80
Jøran Vagnby Lillesand <lillesand@gmail.com>
2015-04-17 15:46:33 +0200

    Moved display functions in override script out of overridden Digipost methods context. This way we only have to add the event listeners once, avoiding the need to remove them for every decrypted document. This will reduce the probablity of multiple event listeners for the same event because we forget to remove them after having displayed a document.

--- plugin/js/override.js
+++ plugin/js/override.js
@@ -1,88 +1,105 @@
-console.log("Overriding Digipost JavaScript functions");
-
 (function() {
 
   "use strict";
 
-  if (dp.url) {
-    dp.url.real_normalize = dp.url.normalize;
+	console.log("Overriding Digipost JavaScript functions");
 
-    dp.url.normalize = function(url) {
-      if (url.indexOf("blob:") === 0) {
-        return url;
-      }
-      return dp.url.real_normalize(url);
-    }
-  }
+	/**
+	 * View where we will show the document once decryption is done.
+	 */
+	var activeContentView;
 
-  if (dp.embed) {
-    dp.embed.real_html = dp.embed.html;
-    dp.embed.html = function(options) {
-      var options = options || {};
-      options.sandbox = '';
-      return dp.embed.real_html(options);
-    }
-  }
+	overrideDigipostJavascriptFunctions();
 
-  if (dp.views.content) {
-    dp.views.content.init_original = dp.views.content.init;
+	document.addEventListener('downloaded', showDecryptionSpinner);
+	document.addEventListener('decrypted', showDecryptedDocument);
+	document.addEventListener('decryption-failed', showEncryptionFailedError);
+
+	function showDecryptionSpinner(e) {
+		dp.spinner.hide();
+		dp.spinner.show({ label: 'Dekrypterer dokument…' });
+		document.dispatchEvent(new CustomEvent('decrypt', {
+			detail: {
+				data: e.detail.data,
+				contentType: e.detail.contentType
+			}
+		}));
+	}
+
+	/**
+	 * Show data as decrypted by the content script
+	 */
+	function showDecryptedDocument(data){
+		dp.spinner.hide();
+		activeContentView.doc.contentUri = function() {
+			return data.detail.url;
+		};
+		activeContentView.imageUrl = activeContentView.doc.contentUri;
+		dp.views.content.init_original.apply(activeContentView);
+		dp.sizes.update($(".doc"));
+		$(".meta h2 a").attr('href', data.detail.url);
+		$("a[download]").attr('href', data.detail.url);
+	}
+
+	function showEncryptionFailedError(data) {
+		dp.spinner.hide();
+		dp.modal.show(data.detail.error);
+	}
+
+	/**
+	 * Override Javascript functions in Digipost frontend code to be able to hook into display logic to decrypt documents in the browser.
+	 */
+	function overrideDigipostJavascriptFunctions() {
+		/**
+		 * Prevent normalization of data URLs. These are browser generated URLs to access Javascript in-memory resources, and should never be modified.
+		 */
+		if (dp.url) {
+			dp.url.real_normalize = dp.url.normalize;
+			dp.url.normalize = function (url) {
+				if (url.indexOf("blob:") === 0) {
+					return url;
+				}
+				return dp.url.real_normalize(url);
+			}
+		}
+
+		/**
+		 * Override embed function to always set the sandbox attribute of the content iframe to an empty string.
+		 * Empty string sandbox attribute enables all sandbox restrictions for the iframe, i.e. content is treated as NOT same origin,
+		 * even though its URL is same origin as the parent page. This is important as data URLs (blobs) are same origin as the parent page.
+		 */
+		if (dp.embed) {
+			dp.embed.real_html = dp.embed.html;
+			dp.embed.html = function (options) {
+				var options = options || {};
+				options.sandbox = '';
+				return dp.embed.real_html(options);
+			}
+		}
 
 		/**
 		 * Trap frontend calls to display contents. If the document is encrypted with a user key,
 		 * dispatch an event to start downloading and decryption.
 		 */
-    dp.views.content.init = function() {
-      if (!this.doc.data.userKeyEncrypted) {
-        // Don't try to decrypt contents if original is not encrypted with user key. Call original content view function.
-        return dp.views.content.init_original.apply(this, arguments);
-      }
-
-      var view = this;
-      dp.spinner.show({ label: 'Laster ned dokument…' });
-      document.addEventListener('downloaded', decrypt);
-      document.addEventListener('decrypted', show);
-      document.addEventListener('decryption-failed', failed);
-
-			// Trigger the content script to start processing the encrypted document.
-      document.dispatchEvent(new CustomEvent('processEncryptedDocument', {
+		if (dp.views.content) {
+			dp.views.content.init_original = dp.views.content.init;
+			dp.views.content.init = function () {
+				if (!this.doc.data.userKeyEncrypted) {
+					// Don't try to decrypt contents if original is not encrypted with user key. Call original content view function.
+					return dp.views.content.init_original.apply(this, arguments);
+				}
+				activeContentView = this;
+				dp.spinner.show({label: 'Laster ned dokument…'});
+				// Trigger the content script to start processing the encrypted document.
+				document.dispatchEvent(new CustomEvent('processEncryptedDocument', {
 					detail: {
 						contentUri: this.doc.contentUri()
 					}
-			}));
+				}));
+			};
+		}
+	}
 
-      function decrypt(e) {
-        document.removeEventListener('downloaded', decrypt);
-        dp.spinner.hide();
-        dp.spinner.show({ label: 'Dekrypterer dokument…' });
-        document.dispatchEvent(new CustomEvent('decrypt', {
-          detail: {
-            data: e.detail.data,
-            contentType: e.detail.contentType
-          }
-        }));
-      }
 
-			/**
-			 * Show data as decrypted by the content script
-			 */
-      function show(data){
-        dp.spinner.hide();
-        document.removeEventListener('decrypted', show);
-        view.doc.contentUri = function() {
-          return data.detail.url;
-        };
-        view.imageUrl = view.doc.contentUri;
-        dp.views.content.init_original.apply(view);
-        dp.sizes.update($(".doc"));
-        $(".meta h2 a").attr('href', data.detail.url);
-        $("a[download]").attr('href', data.detail.url);
-      }
-
-      function failed(data) {
-        dp.spinner.hide();
-        dp.modal.show(data.detail.error);
-      }
-    };
-  }
 
 })();
