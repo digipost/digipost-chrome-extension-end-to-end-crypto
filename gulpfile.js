cf66c7ffbe92d13c04b9e154330c0c9c36ad5051
Jøran Vagnby Lillesand <lillesand@gmail.com>
2015-04-22 11:37:43 +0200

    Added support for packaging extension

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 
 var jshint = require('gulp-jshint');
 var taskListing = require('gulp-task-listing');
+var zip = require('gulp-zip');
 
 // Add a task to render the output
 
@@ -9,6 +10,19 @@ gulp.task('lint', function() {
 	return gulp.src('extension/js/*.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'));
+});
+
+gulp.task('package', function() {
+	return gulp.src('extension/**')
+		.pipe(zip('extension.zip'))
+		.pipe(gulp.dest('dist'))
+		.pipe(function() {
+			return {
+				on: function() {
+					console.log(arguments);
+				}
+			}
+		});
 });
 
 gulp.task('default', taskListing);
