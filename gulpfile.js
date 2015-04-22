daca1dea0874e6d8853040ef9a61d943fb510ef7
Jøran Vagnby Lillesand <lillesand@gmail.com>
2015-04-22 11:40:22 +0200

    Clean dist folder before packaging new

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var taskListing = require('gulp-task-listing');
 var zip = require('gulp-zip');
+var clean = require('gulp-clean');
 
 // Add a task to render the output
 
@@ -12,12 +13,15 @@ gulp.task('lint', function() {
 		.pipe(jshint.reporter('default'));
 });
 
-gulp.task('package', function() {
+gulp.task('package', ['clean'], function() {
 	return gulp.src('extension/**')
 		.pipe(zip('extension.zip'))
 		.pipe(gulp.dest('dist'))
 });
 
-
+gulp.task('clean', function() {
+	return gulp.src('dist/', { read: false })
+		.pipe(clean());
+});
 
 gulp.task('default', taskListing);
